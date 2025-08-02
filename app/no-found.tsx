887cb8ffe557160634843a958f4fbacbb8488e1e
dev-solano <lsolano@marfilbsc.com>
2025-08-02 13:21:37 -0500

    latest Chaper 3

---
+++ app/no-found.tsx
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function NoFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
+      <View style={styles.container}></View>
+      <Link href="/" style={styles.button}>
+        Go back to Home Screen!
+      </Link>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
